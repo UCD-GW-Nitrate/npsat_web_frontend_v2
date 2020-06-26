09508c43c7a81b65d8678446680b1561babd98f1
Zesheng Xing <44153625+zsxing99@users.noreply.github.com>
2020-06-26 14:06:15 -0700

    -remove umi cache

--- src/.umi/plugin-dva/exports.ts
+++
@@ -1,4 +0,0 @@
-// @ts-nocheck
-
-export { connect, useDispatch, useStore, useSelector } from 'D:/work/UCD/DiSSC_2020/front_end/npsat/node_modules/dva';
-export { getApp as getDvaApp } from './dva';
--- src/.umi/plugin-helmet/exports.ts
+++
@@ -1,3 +0,0 @@
-// @ts-nocheck
-// @ts-ignore
-export { Helmet } from 'D:/work/UCD/DiSSC_2020/front_end/npsat/node_modules/react-helmet';
