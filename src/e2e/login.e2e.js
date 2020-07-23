173bdd736c2e131138fa44505ee7650c75e3b68c
Zesheng Xing <44153625+zsxing99@users.noreply.github.com>
2020-07-23 15:20:21 -0700

    -add mock data test

--- src/e2e/login.e2e.js
+++ src/e2e/login.e2e.js
@@ -1,8 +1,10 @@
 import { BASE_URL } from '@/e2e/config';
 import { username, password } from '@/e2e/local_config';
 
-describe('Login page smoke test suits', () => {
+describe('Login page smoke test suits with backend support', () => {
   it('footer', async () => {
+    // eslint-disable-next-line no-undef
+    const page = await browser.newPage();
     await page.goto(`${BASE_URL}/user/login`);
     await page.waitForSelector('footer', {
       timeout: 2000,
@@ -10,10 +12,13 @@ describe('Login page smoke test suits', () => {
     const haveFooter = await page.evaluate(
       () => document.getElementsByTagName('footer').length > 0,
     );
+    await page.close();
     expect(haveFooter).toBeTruthy();
   });
 
   it('Login fail', async () => {
+    // eslint-disable-next-line no-undef
+    const page = await browser.newPage();
     await page.goto(`${BASE_URL}/user/login`);
     await page.type('#userName', 'faker');
     await page.type('#password', 'LOL-Faker-password');
@@ -22,15 +27,19 @@ describe('Login page smoke test suits', () => {
     const loginFail = await page.evaluate(
       () => document.getElementsByClassName('ant-alert-error').length === 1,
     );
+    await page.close();
     expect(loginFail).toBeTruthy();
   });
 
   it('Login success', async () => {
+    // eslint-disable-next-line no-undef
+    const page = await browser.newPage();
     await page.goto(`${BASE_URL}/user/login`);
     await page.type('#userName', username);
     await page.type('#password', password);
-    await page.click('button[type="submit"]');
+    await Promise.all([page.waitForNavigation(), page.click('button[type="submit"]')]);
     const url = await page.url();
-    expect(url).toBe(`${BASE_URL}/dashboard`);
+    await page.close();
+    expect(url).toBe(`${BASE_URL}/`);
   });
 });
