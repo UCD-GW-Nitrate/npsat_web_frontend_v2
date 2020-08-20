7ae53567fb0cbdfcae574b204d0dadcf2eed881f
Zesheng Xing <44153625+zsxing99@users.noreply.github.com>
2020-08-20 11:02:43 -0700

    -remove adoption rate

--- src/pages/model/view/components/ModelDetail/components/TableWrapper/index.jsx
+++ src/pages/model/view/components/ModelDetail/components/TableWrapper/index.jsx
@@ -22,15 +22,15 @@ const TableWrapper = props => {
           ),
           sorter: (a, b) => parseFloat(a.proportion) - parseFloat(b.proportion)
         },
-        {
-          title: "Land area percentage",
-          dataIndex: 'land_area_proportion',
-          key: 'land_area_proportion',
-          render: num => (
-            `${Math.round(parseFloat(num) * 100)}%`
-          ),
-          sorter: (a, b) => parseFloat(a.land_area_proportion) - parseFloat(b.land_area_proportion)
-        },
+        // {
+        //   title: "Land area percentage",
+        //   dataIndex: 'land_area_proportion',
+        //   key: 'land_area_proportion',
+        //   render: num => (
+        //     `${Math.round(parseFloat(num) * 100)}%`
+        //   ),
+        //   sorter: (a, b) => parseFloat(a.land_area_proportion) - parseFloat(b.land_area_proportion)
+        // },
       ]}
       rowKey={modification => modification.crop}
       pagination={false}
