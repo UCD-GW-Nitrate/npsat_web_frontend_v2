3564dd96de6043abce39c962ffcbcc9038e13d14
Zesheng Xing <44153625+zsxing99@users.noreply.github.com>
2020-09-21 11:26:38 -0700

    base comparison page

--- src/pages/results/compare/index.jsx
+++ src/pages/results/compare/index.jsx
@@ -1,25 +1,64 @@
-import React, { useEffect } from 'react';
-import { useLocation, connect } from "umi";
+import React, { useEffect, useState } from 'react';
+import { useLocation, connect, history } from 'umi';
 import NoFoundPage from '@/pages/404';
+import { notification } from 'antd';
+import { getModelAndBaseModel } from '@/pages/results/service';
 
 const ResultCompare = props => {
   const location = useLocation();
   const { user } = props;
-  const { ids = null } = location.query;
-  const models = decodeURI(ids).split(" ");
-  console.log(models)
-  if (!ids) {
+  const { token } = user;
+  const [ info, setInfo ] = useState({});
+  const { id = null } = location.query;
+  useEffect(() => {
+    if (id === null) {
+      notification.warn({
+        message: "Please choose a model to compare with the base model",
+        description: "No model selected",
+        duration: 8
+      });
+      // set 5s timer
+      const redirectTimer = setTimeout(() => {
+        history.push({
+          pathname: '/model/overview'
+        });
+      }, 5000);
+      // componentWillUnmount
+      // avoid redirection after leaving current page
+      return () => clearTimeout(redirectTimer);
+    }
+    (async () => {
+      const model = await getModelAndBaseModel( { id }, token);
+      if (typeof model === "string" && model.startsWith("ERROR")) {
+        setInfo({ error: model });
+        return true;
+      } else {
+        setInfo(model);
+        return false;
+      }
+    })();
+  }, [id]);
+
+  if (!id) {
     return (
       <NoFoundPage
-        subTitle="No model selected for comparison"
-        title="No model selected"
-        redirection="/model/select"
-        buttonText="Select model(s)"
+        subTitle="This page will be redirected in 5 seconds"
+        title="No model specified"
+        redirection="/model/overview"
+        buttonText="Select model"
+      />)
+  } else if (info.error) {
+    return (
+      <NoFoundPage
+        subTitle={`The model id with ${id} inaccessible`}
+        title="The model you look for is private or cannot be found"
+        redirection="/model/overview"
+        buttonText="Select model"
       />
     )
+  } else {
+    return null;
   }
-
-  return null;
 }
 
 export default connect(({ user }) => ({
---
+++ src/pages/results/service.js
@@ -0,0 +1,9 @@
+import request from '@/utils/request';
+
+export async function getModelAndBaseModel(params, token) {
+  return request(`/api/model_run/${params.id}/`, {
+    method: 'GET',
+    headers: { 'Authorization': `Token ${token}` },
+    data: { id: params.id },
+  });
+}
